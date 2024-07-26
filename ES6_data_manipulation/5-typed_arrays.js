c676b7ec5e6e7adabd0b61d271dafe8e3e2a0c1e
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-07-26 13:08:29 +0200

    imporved logic for making int8Array and satisfied eslint

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
-  const typedArr = new Int8Array(length);
-
-  typedArr[position] = value;
+  const typedArr = new Int8Array(() => {
+    const arr = [];
+    for (let i = 0; i < length; i += 1) {
+      if (i !== position) arr.push(0);
+      else arr.push(value);
+    }
+    return arr;
+  });
 
   return typedArr;
 }
