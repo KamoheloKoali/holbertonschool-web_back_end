dcb04ad4f7575e372ec104dd5878ccad5f1a84a3
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-08-14 15:35:09 +0200

    python annotation

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+import math
+
+
+def floor(n: float) -> int:
+    """
+        return the floor of n
+        
+        Args:
+            n (float): decimal number
+        
+        Returns:
+            the floor of n
+    """
+    
+    return math.floor(n)    
