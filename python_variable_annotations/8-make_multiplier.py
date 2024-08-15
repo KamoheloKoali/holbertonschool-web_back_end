22632f165176a54569a8efdf5dde9b93f6039dde
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-08-15 11:29:11 +0200

    python annotation

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -8,7 +8,7 @@ from typing import Callable
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
         returns a function that multiplies a float by multiplier
-        Args: 
+        Args:
             multiplier (float): decimal number
         Returns:
             a function that multiplies a float by multiplier
