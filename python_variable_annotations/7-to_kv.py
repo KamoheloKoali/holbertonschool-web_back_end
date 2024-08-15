925c8218b1bf6ea81e501c0401fc04339d65d163
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-08-15 11:27:03 +0200

    python annotation

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -8,7 +8,7 @@ from typing import Union, Tuple
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
         returns a tuple
-        Args: 
+        Args:
             k (str): string
             v (int/float): number
         Returns:
