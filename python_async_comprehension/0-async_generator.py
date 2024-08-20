7162f237e5434fd8ef077fa920bfc45eb9c3dbf0
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-08-20 11:21:39 +0200

    async comprehension, AsyncIterator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+"""
+    contains the function async_generator
+    that yeilds a random number every second
+
+    Imports:
+        - asyncio
+        - typing
+"""
+
+import asyncio
+from typing import AsyncIterator
+import random
+
+
+async def async_generator() -> AsyncIterator[int]:
+    """
+        Yields a random number every second
+    """
+    for num in range(10):
+        yield random.choice(range(10))
+        await asyncio.sleep(1)
